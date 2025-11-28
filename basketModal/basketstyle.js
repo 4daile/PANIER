0166cb0cb6a232c3fe54539087bed0b2ac9bd2c3
4daile <abouvard@esad-orleans.fr>
2025-11-28 12:57:04 +0100

    js button!!!!!! yes

--- basketModal/basketstyle.js
+++ basketModal/basketstyle.js
@@ -1,15 +1,23 @@
 console.log("hello worlds")
+
 let myButton = document.querySelector(".open");
 myButton.addEventListener("click", function() {
     openPanneau();
 })
 
+let closeButton = document.querySelector(".close");
+closeButton.addEventListener("click", function() {
+    closePanneau();
+})
+
 function openPanneau(){
-    console.log("openPanneau")
-    const panneau = document.getElementsByClassName("panneau");
-    if (panneau > 0) {
-        console.log("panneau trouvé")
-    } else {
-        console.log(":(((")
-    }
+    document.querySelector(".panneau").classList.add("active")
+    //console.log("panneau")
+    //console.log("active")
 }
+
+function closePanneau(){
+    document.querySelector(".panneau").classList.remove("active")
+}
+
+
